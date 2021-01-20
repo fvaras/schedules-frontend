5a5236df03ccdb94b6c3cce228aaa32228b38741
fvaras <fdovarasc@gmail.com>
2021-01-20 00:23:06 -0300

    reactjs

---
+++ schedules-frontend-reactjs/src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import Schedule from './components/Schedule'
+import io from 'socket.io-client';
+
+const connectSocket = () => {
+  const socket = io.connect('https://fvaras-schedules-back-nodejs.herokuapp.com/')
+  // const socket = io.connect('http://localhost:3001')
+  return socket
+}
+
+const App = () => {
+
+  const [socket] = useState(connectSocket())
+  const [schedules, setSchedules] = useState([])
+  const [isFull, setIsFull] = useState(false)
+
+  useEffect(() => {
+    socket.on('update', data => {
+      console.log('update schedules', data)
+      const { schedules, isFull } = data
+      setSchedules(schedules)
+      setIsFull(isFull)
+    })
+  }, [socket])
+
+  const setSchedule = schedule => {
+    const { isAvailable, user } = schedule
+    const { id: socketId } = socket
+
+    if (!isAvailable && user != socketId) {
+      alert('only the owner of this schedule can release the resource')
+      return
+    }
+
+    if (isFull && user !== socketId) {
+      alert('We reach the full capacity, please wait until a resource is available')
+      return
+    }
+
+    schedule.isAvailable = !schedule.isAvailable
+    socket.emit('setSchedule', schedule)
+  }
+
+  return (
+    <div className="container">
+      <div className="row mt-5">
+        {schedules.map(schedule => (
+          <Schedule
+            key={schedule.id}
+            schedule={schedule}
+            setSchedule={setSchedule}
+            isFull={isFull}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ schedules-frontend-reactjs/src/components/Schedule.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import format from 'date-fns/format'
+
+const Schedule = ({ schedule, setSchedule, isFull }) => {
+    const { start, end, isAvailable } = schedule
+
+    let btnClass = "btn btn-lg "
+    if (!isAvailable)
+        btnClass = btnClass.concat('btn-success')
+    else {
+        if (isFull)
+            btnClass = btnClass.concat('btn-danger')
+        else
+            btnClass = btnClass.concat('btn-primary')
+    }
+    return (
+        <div className="col-md-3 mb-2">
+            <button type="button" className={btnClass} onClick={() => setSchedule(schedule)}>
+                {format(new Date(start), 'HH:mm')} - {format(new Date(end), 'HH:mm')}
+            </button>
+        </div>
+    )
+}
+
+export default Schedule
